5be47fe82da0bde22bd8be9e6ce0ca2275a97d2f
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2023-05-16 16:34:30 -0300

    Updated

--- C#/C#_Como_Programar/Destructor.cs
+++ C#/C#_Como_Programar/Destructor.cs
@@ -7,7 +7,7 @@ class DestructorClass
     ~DestructorClass() // Método com nome igual da classe precedido por um "~" se torna um destructor.
     {
         // Código de liberação de recursos;
-        // Base.Finalize() é chamado automaticamente em C#. 
+        // (Base.)Finalize() é chamado automaticamente em C#. 
     }
 }
 
@@ -15,6 +15,15 @@ class Program
 {
     static void Main()
     {
+        DestructorClass TDC;
+        TDC = new DestructorClass();
+
         Console.WriteLine();
+
+        // Para coleta de lixo.
+        TDC = null;
+
+        // Informa o coletor de lixo para que seja executado.
+        System.GC.Collect();
     }
 }
