63dd596fa155e3d4c8981a11b220c33cd9f463c0
DevJonathanMendes <jonathanbarreiro88@gmail.com>
2023-05-19 18:12:07 -0300

    Initial commit

---
+++ C#/C#_Como_Programar/Capítulo 4 - Estrutura de Controle 1/4.11-Operators-Assignment.cs
@@ -0,0 +1,22 @@
+// Fig: 4.11: Operadores de atribuição.
+// Uma forma de abreviação.
+
+using System;
+
+static class Program
+{
+	static void Main(string[] args)
+	{
+		int variable = 0;
+
+		variable = variable + 2; // Pode ser abreviada.
+		variable += 2;
+		variable -= 2;
+		variable *= 2;
+		variable /= 2;
+		variable %= 2;
+
+		Console.Clear();
+		Console.WriteLine(variable);
+	}
+}
